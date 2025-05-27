2a7d64478e69a33e6ebc439b501de369bf8bd616
Oleg Vorontsov <oleg-voroncov@mail.ru>
2025-05-27 09:41:06 +0300

    добавлены DBcontext, файлы конфигураций сущностей

---
+++ DirectoryService/src/DirectoryService.Infrastructure/DataBase/ApplicationDBContext.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DirectoryService.Infrastructure.DataBase;
+
+public class ApplicationDBContext : DbContext
+{
+    public const string DATABASE_CONFIGURATION = "Database";
+
+    private readonly string _connectionString;
+
+    public ApplicationDBContext(
+        string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(_connectionString);
+        optionsBuilder.LogTo(Console.WriteLine);
+        optionsBuilder.UseSnakeCaseNamingConvention();
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("directory_service");
+
+        modelBuilder.HasPostgresExtension("ltree");  // used in entities: Department
+
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(ApplicationDBContext).Assembly,
+            type => type.FullName?.Contains("Database.Configurations") ?? false);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ DirectoryService/src/DirectoryService.Infrastructure/DataBase/Configurations/LocationConfiguration.cs
@@ -0,0 +1,82 @@
+using DirectoryService.Domain.Models;
+using DirectoryService.Domain.Shared.BaseClasses;
+using DirectoryService.Domain.ValueObjects.LocationValueObjects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DirectoryService.Infrastructure.DataBase.Configurations;
+
+public class LocationConfiguration : IEntityTypeConfiguration<Location>
+{
+    public void Configure(EntityTypeBuilder<Location> builder)
+    {
+        builder.ToTable("locations");
+
+        builder.HasKey(d => d.Id);
+        builder.Property(d => d.Id)
+               .HasConversion(
+                   id => id.Value,
+                   value => Id<Location>.Create(value))
+               .HasColumnName("id");
+
+        builder.Property(d => d.IsActive)
+               .IsRequired()
+               .HasColumnName("is_active");
+        builder.HasQueryFilter(d => d.IsActive); // global query filter
+
+        builder.Property(d => d.Name)
+               .HasConversion(
+                   name => name.Value,
+                   value => LocationName.Create(value).Value!)
+               .IsRequired()
+               .HasMaxLength(LocationName.MAX_LENGTH)
+               .HasColumnName("name");
+
+        builder.HasIndex(d => d.Name)
+               .IsUnique();
+
+        builder.OwnsOne(d => d.Address, ib =>
+        {
+            ib.ToJson("address");
+
+            ib.Property(i => i.City)
+                .IsRequired()
+                .HasMaxLength(LocationAddress.CITY_MAX_LENGTH);
+
+            ib.Property(i => i.Street)
+                .IsRequired()
+                .HasMaxLength(LocationAddress.STREET_MAX_LENGTH);
+
+            ib.Property(i => i.HouseNumber)
+                .IsRequired()
+                .HasMaxLength(LocationAddress.HOUSENUMBER_MAX_LENGTH);
+        });
+
+        builder.ComplexProperty(d => d.TimeZone, ib =>
+        {
+            ib.Property(i => i.Value)
+                .IsRequired()
+                .HasMaxLength(IANATimeZone.MAX_LENGTH)
+                .HasColumnName("time_zone");
+        });
+
+        builder.HasMany(l => l.DepartmentLocations)
+               .WithOne(dl => dl.Location)
+               .HasForeignKey(dl => dl.LocationId)
+               .OnDelete(DeleteBehavior.Cascade);
+
+        builder.Property(d => d.CreatedAt)
+               .HasConversion(
+                   src => src.Kind == DateTimeKind.Utc ? src : DateTime.SpecifyKind(src, DateTimeKind.Utc),
+                   dst => dst.Kind == DateTimeKind.Utc ? dst : DateTime.SpecifyKind(dst, DateTimeKind.Utc))
+               .IsRequired()
+               .HasColumnName("created_at");
+
+        builder.Property(d => d.UpdatedAt)
+               .HasConversion(
+                   src => src.Kind == DateTimeKind.Utc ? src : DateTime.SpecifyKind(src, DateTimeKind.Utc),
+                   dst => dst.Kind == DateTimeKind.Utc ? dst : DateTime.SpecifyKind(dst, DateTimeKind.Utc))
+               .IsRequired()
+               .HasColumnName("updated_at");
+    }
+}
