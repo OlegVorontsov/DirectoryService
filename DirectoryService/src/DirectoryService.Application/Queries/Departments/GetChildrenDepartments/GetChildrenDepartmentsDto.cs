8435d52bd27849cb7c5f5ae4f1655bd64f15c2d4
Oleg Vorontsov <oleg-voroncov@mail.ru>
2025-09-23 10:38:04 +0300

    получение дочерних департаментов через кэш

---
+++ DirectoryService/src/DirectoryService.Application/Queries/Departments/GetChildrenDepartments/GetChildrenDepartmentsDto.cs
@@ -0,0 +1,5 @@
+﻿using DirectoryService.Application.Shared.DTOs;
+
+namespace DirectoryService.Application.Queries.Departments.GetChildrenDepartments;
+
+public record GetChildrenDepartmentsDto(int TotalCount, IEnumerable<DepartmentDTO> Departments);
